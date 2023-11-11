21662e9771144accebe75b7daf9056c23c6b414d
young woo <tedious2@naver.com>
2023-11-11 14:43:45 +0900

    youngwoo_commit_1111_1443_이메일방송이력view수정

--- src/main/java/com/dev/pilafix/admin/sendemailhistory/SendEmailHistoryService.java
+++ src/main/java/com/dev/pilafix/admin/sendemailhistory/SendEmailHistoryService.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 public interface SendEmailHistoryService {
 	List<SendEmailHistoryVO> getSendEmailHistoryInfoList();
-	SendEmailHistoryVO getSendEmailHistoryInfo(int mhEmailSendCode);
-	int insertSendEmailHistoryInfo(SendEmailHistoryVO vo);
-	int updateSendEmailHistoryInfo(SendEmailHistoryVO vo);
-	int deleteSendEmailHistoryInfo(int mhEmailSendCode);
+	SendEmailHistoryVO getSendEmailHistoryInfo(String mhEmailSendCode);
 	
 }
