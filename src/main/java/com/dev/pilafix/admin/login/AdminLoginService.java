e05664b04940916146b91370f74e725bbbf842d7
young woo <tedious2@naver.com>
2023-11-18 14:41:17 +0900

    youngwoo_commit_1118_1441_admin로그인구현

--- src/main/java/com/dev/pilafix/admin/login/AdminLoginService.java
+++ src/main/java/com/dev/pilafix/admin/login/AdminLoginService.java
@@ -6,5 +6,11 @@ public interface AdminLoginService {
 	
 
 	AdminVO adminLogin(String adId, String adPassword);
-	 
+
+	int insertAdminRegister(AdminVO vo);
+
+	int adIdCheck(String adId);
+	
+	void adminupdatePassword(String adCode, String newPassword);
+
 }
