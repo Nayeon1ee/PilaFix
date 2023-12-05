2973ddc323fc633e7b4c0aa2fe56080446e59805
Juminseung <wrwr05@naver.com>
2023-12-05 14:46:19 +0900

    minseung commit 1205 1446 알림 영역

--- src/main/webapp/resources/js/notice.js
+++ src/main/webapp/resources/js/notice.js
@@ -5,32 +5,32 @@ var csMemberCode = parseInt(document.getElementById('csMemberCode').getAttribute
 
 $(document).ready(function() {
     csMemberCode = document.getElementById('csMemberCode').getAttribute('data-cs-member-code'); //세션에서 코드 가져옴 
-	console.log(csMemberCode);
+   console.log(csMemberCode);
     
-	// 로그인이 되었을 때만 setInterval 타이머 설정
+   // 로그인이 되었을 때만 setInterval 타이머 설정
     if (csMemberCode !== null && csMemberCode !== 0) {
         // 타이머로 3초 설정하여 AJAX로 DB 정보 요청
-    	setInterval(function() {
-    		var notificationBadge = $('#NotificationBadge');
-    	    // AJAX를 이용하여 서버에 정보 요청
-    	    $.ajax({
-    	        url: 'unReadNotificationCount.do',
-    	        method: 'POST',
-    	        data: { csMemberCode: csMemberCode },
-    	        success: function(response) {
-    	            // 서버로부터의 응답 처리
-    				if (response > 0) {
-    					notificationBadge.empty();
-    		            notificationBadge.html('<span class="badge rounded-pill bg-danger">' + response + '</span>');
-    			    } else {
-    			        notificationBadge.empty();
-    			    }
-    	        },
-    	        error: function(error) {
-    	            console.error('AJAX 요청 시 에러', error);
-    	        }
-    	    });
-    	}, 30000000); // 3초에 한 번씩 실행 추후에 3000으로 바꾸기
+       setInterval(function() {
+          var notificationBadge = $('#NotificationBadge');
+           // AJAX를 이용하여 서버에 정보 요청
+           $.ajax({
+               url: 'unReadNotificationCount.do',
+               method: 'POST',
+               data: { csMemberCode: csMemberCode },
+               success: function(response) {
+                   // 서버로부터의 응답 처리
+                if (response > 0) {
+                   notificationBadge.empty();
+                      notificationBadge.html('<span class="badge rounded-pill bg-danger">' + response + '</span>');
+                 } else {
+                     notificationBadge.empty();
+                 }
+               },
+               error: function(error) {
+                   console.error('AJAX 요청 시 에러', error);
+               }
+           });
+       }, 3000); // 3초에 한 번씩 실행 추후에 3000으로 바꾸기
     }
 });
 
@@ -45,58 +45,58 @@ const DEFAULT_CENTER_NAME = '필라픽스';
 
 //종 버튼 클릭 시 동작하는 함수
 function handleNotificationClick() {
-	csMemberCode = document.getElementById('csMemberCode').getAttribute('data-cs-member-code'); //세션에서 코드 가져옴 
-	
-	$.ajax({
-	        url: 'getUnReadNotification.do',
-	        method: 'POST',
-	        data: { csMemberCode: csMemberCode },
-	        success: function(response) {
-	        	console.log("success 수신 ");
-	            console.log(response);
-	            
-	            $('#NotificationsList').html('');
-	            
-	            if (response.length < 1) {
-	                $('#NotificationsList').append('<div class="dropdown-item">' + NO_NOTIFICATION_MESSAGE + '</div>');
-	            } else {
-	            	 response.forEach(function (item) {
-	                    let eventType = item.eventType; 
-						let ncId = item.ncId;
-						let ncReadYn = item.ncReadYn;
-						
-						console.log("읽음여부");
-						console.log(item.ncReadYn);
-						
-						if(ncReadYn){ //사용자가 확인했다면 
-							var gstr = '<a class="dropdown-item" style="background-color: #f8f9fa;"  onclick="requestView(\''+ eventType + '\')">'; 
-						}else{
-							var gstr = '<a class="dropdown-item" style="background-color: #f8f4ff;" onclick="updateNoticeStatus(\'' + ncId + '\',\'' + eventType + '\')">'; //여기서 onclick 시에 화면이동+함수호출해야 함 호출 시 item.ncId 넣어야 함
-						}
-						gstr += '<div>';
-	
-	                    if (item.centerName !== null) {
-	                        gstr += '<small class="text-muted">' + item.centerName + '</small>'; // 센터이름이 존재한다면
-	                    } else {
-	                        gstr += '<small class="text-muted">' + DEFAULT_CENTER_NAME + '</small>'; // 관리자이면
-	                    }
-	
-	                    gstr += '<div class="d-flex w-100 justify-content-between">';
-	                    gstr += '<h5 class="mb-1">' + item.ncNoticeContent + '</h5>';
-	                    gstr += '</div>';
-	                    gstr += '<small class="text-muted">' + formatTimestamp(item.eventDatetime) + '</small>';
-	                    gstr += '</div>';
-	                    gstr += '</a>';
-	
-	
-	                    $('#NotificationsList').append(gstr);
-	                });
-			    }
-	        },
-	        error: function(error) {
-	            console.error('AJAX 요청 시 에러', error);
-	        }
-	    });
+   csMemberCode = document.getElementById('csMemberCode').getAttribute('data-cs-member-code'); //세션에서 코드 가져옴 
+   
+   $.ajax({
+           url: 'getUnReadNotification.do',
+           method: 'POST',
+           data: { csMemberCode: csMemberCode },
+           success: function(response) {
+              console.log("success 수신 ");
+               console.log(response);
+               
+               $('#NotificationsList').html('');
+               
+               if (response.length < 1) {
+                   $('#NotificationsList').append('<div class="dropdown-item">' + NO_NOTIFICATION_MESSAGE + '</div>');
+               } else {
+                   response.forEach(function (item) {
+                       let eventType = item.eventType; 
+                  let ncId = item.ncId;
+                  let ncReadYn = item.ncReadYn;
+                  
+                  console.log("읽음여부");
+                  console.log(item.ncReadYn);
+                  
+                  if(ncReadYn){ //사용자가 확인했다면 
+                     var gstr = '<a class="dropdown-item" style="background-color: #f8f9fa;"  onclick="requestView(\''+ eventType + '\')">'; 
+                  }else{
+                     var gstr = '<a class="dropdown-item" style="background-color: #f8f4ff;" onclick="updateNoticeStatus(\'' + ncId + '\',\'' + eventType + '\')">'; //여기서 onclick 시에 화면이동+함수호출해야 함 호출 시 item.ncId 넣어야 함
+                  }
+                  gstr += '<div>';
+   
+                       if (item.centerName !== null) {
+                           gstr += '<small class="text-muted">' + item.centerName + '</small>'; // 센터이름이 존재한다면
+                       } else {
+                           gstr += '<small class="text-muted">' + DEFAULT_CENTER_NAME + '</small>'; // 관리자이면
+                       }
+   
+                       gstr += '<div class="d-flex w-100 justify-content-between">';
+                       gstr += '<h5 class="mb-1">' + item.ncNoticeContent + '</h5>';
+                       gstr += '</div>';
+                       gstr += '<small class="text-muted">' + formatTimestamp(item.eventDatetime) + '</small>';
+                       gstr += '</div>';
+                       gstr += '</a>';
+   
+   
+                       $('#NotificationsList').append(gstr);
+                   });
+             }
+           },
+           error: function(error) {
+               console.error('AJAX 요청 시 에러', error);
+           }
+       });
 }
 
 
@@ -104,33 +104,33 @@ function handleNotificationClick() {
 
 // 알림 클릭 시 알림테이블 확인여부 업데이트 
 function updateNoticeStatus(ncId, eventType){
-	console.log('Received ncId:', ncId);
+   console.log('Received ncId:', ncId);
     console.log('Received eventType:', eventType);
-	
-	$.ajax({
-	        url: 'updateNoticeStatus.do',
-	        method: 'POST',
-	        data: { 
-				ncId: ncId,
-				eventType : eventType
-			 },
-	        success: function(response) {
-	        	console.log("확인여부 업데이트 완료");
-				requestView(eventType);
-	        },
-	        error: function(error) {
-	            console.error('AJAX 요청 시 에러', error);
-	        }
-	    });
+   
+   $.ajax({
+           url: 'updateNoticeStatus.do',
+           method: 'POST',
+           data: { 
+            ncId: ncId,
+            eventType : eventType
+          },
+           success: function(response) {
+              console.log("확인여부 업데이트 완료");
+            requestView(eventType);
+           },
+           error: function(error) {
+               console.error('AJAX 요청 시 에러', error);
+           }
+       });
 }
 
 // 확인한 알림 클릭 시 화면 이동 
 function requestView(eventType){
-	if(eventType === '예약' || eventType  === '예약취소'){ //예약이거나 예약취소이면 내스케줄 페이지
-		window.location.href='schedule.do';
-	}else{
-		window.location.href='myNoticePage.do'; //추후 공지사항으로 이동 
-	}
+   if(eventType === '예약' || eventType  === '예약취소'){ //예약이거나 예약취소이면 내스케줄 페이지
+      window.location.href='schedule.do';
+   }else{
+      window.location.href='myNoticePage.do'; //추후 공지사항으로 이동 
+   }
 }
 
 
@@ -145,5 +145,4 @@ function formatTimestamp(timestamp) {
 
     return `${year}.${month}.${day} ${hours}:${minutes}`;
 }
-
 
